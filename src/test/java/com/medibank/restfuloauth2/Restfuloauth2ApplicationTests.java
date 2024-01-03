abe3b27976002e1b2fff3f29a79c0c344375820d
Jeffry Susanto <jeffry.susanto@medibank.com.au>
2024-01-03 16:51:22 +1100

    Initial commit on testing Restful OATH2 - Java 17, netbeans IDE

---
+++ src/test/java/com/medibank/restfuloauth2/Restfuloauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medibank.restfuloauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restfuloauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
